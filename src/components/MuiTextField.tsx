08b8548b8b2dc300180ee481d96fc54d50dd02ff
Manik <tonysins43@gmail.com>
2023-07-21 15:23:28 +0600

    Mui button component added

--- src/components/MuiTextField.tsx
+++ src/components/MuiTextField.tsx
@@ -1,28 +1,59 @@
-import { Stack, TextField } from '@mui/material'
-import React from 'react'
+import { InputAdornment, Stack, TextField} from "@mui/material";
+import React from "react";
 
 const MuiTextField = () => {
   return (
     <Stack spacing={4}>
-        // variant in input field------------------
-        <Stack direction="row" spacing={2}>
-            <TextField label="Name" variant='outlined'></TextField>
-            <TextField label="Name" variant='filled'></TextField>
-            <TextField label="Name" variant='standard'></TextField>
-        </Stack>
-        // size and color ------------------
-        <Stack direction="row" spacing={2}>
-        <TextField label="Small Secondary" size='small' color='secondary'></TextField>
-        <TextField label="Medium Success" size='medium' color='success'></TextField>
-        </Stack>
-        // required , type , helperText , readOnly--------
-        <Stack direction="row" spacing={2}>
+      // variant in input field------------------
+      <Stack direction="row" spacing={2}>
+        <TextField label="Name" variant="outlined"></TextField>
+        <TextField label="Name" variant="filled"></TextField>
+        <TextField label="Name" variant="standard"></TextField>
+      </Stack>
+      // size and color ------------------
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Small Secondary"
+          size="small"
+          color="secondary"
+        ></TextField>
+        <TextField
+          label="Medium Success"
+          size="medium"
+          color="success"
+        ></TextField>
+      </Stack>
+      // required , type , helperText , readOnly--------
+      <Stack direction="row" spacing={2}>
         <TextField label="Form Input" required></TextField>
-        <TextField label="Password" type='password' required helperText='Do not share you password with anyone'></TextField>
-        <TextField label="Read Only" inputProps={{readOnly:true}}></TextField>
-        </Stack>
-    </Stack>
-  )
-}
+        <TextField
+          label="Password"
+          type="password"
+          required
+          helperText="Do not share you password with anyone"
+        ></TextField>
+        <TextField
+          label="Read Only"
+          inputProps={{ readOnly: true }}
+        ></TextField>
+      </Stack>
 
-export default MuiTextField
+
+     // Adornments------------
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Amount"
+         inputProps={{startAdornment:<InputAdornment position="start">$</InputAdornment>}}
+        ></TextField>
+        <TextField
+          label="Weight"
+          inputProps={{
+            endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+          }}
+        ></TextField>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiTextField;
